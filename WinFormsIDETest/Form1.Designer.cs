1aac44ac056f16b907ecdb403e5b55cc3d4182e2
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-09-09 16:08:42 -0700

    cleaning, autosizing

--- WinFormsIDETest/Form1.Designer.cs
+++ WinFormsIDETest/Form1.Designer.cs
@@ -50,6 +50,7 @@
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
             this.ResumeLayout(false);
 
         }
--- WinFormsIDETest/Form1.cs
+++ WinFormsIDETest/Form1.cs
@@ -12,6 +12,8 @@ namespace WinFormsIDETest
 {
     public partial class Form1 : Form
     {
+        Size Rel;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,6 +23,7 @@ namespace WinFormsIDETest
             t.Start();
             richTextBox1.AcceptsTab = true;
             richTextBox1.HScroll += RichTextBox1_HScroll;
+            Rel = this.Size - richTextBox1.Size;
         }
 
         private void RichTextBox1_HScroll(object sender, EventArgs e)
@@ -32,26 +35,18 @@ namespace WinFormsIDETest
 
         public static void Suspend(Control control)
         {
-            Message msgSuspendUpdate = Message.Create(control.Handle, WM_SETREDRAW, IntPtr.Zero,
-                IntPtr.Zero);
-
-            NativeWindow window = NativeWindow.FromHandle(control.Handle);
-            window.DefWndProc(ref msgSuspendUpdate);
+            Message msgSuspendUpdate = Message.Create(control.Handle, WM_SETREDRAW, IntPtr.Zero, IntPtr.Zero);
+            NativeWindow.FromHandle(control.Handle).DefWndProc(ref msgSuspendUpdate);
         }
 
         public static void Resume(Control control)
         {
-            // Create a C "true" boolean as an IntPtr
-            IntPtr wparam = new IntPtr(1);
-            Message msgResumeUpdate = Message.Create(control.Handle, WM_SETREDRAW, wparam,
-                IntPtr.Zero);
-
-            NativeWindow window = NativeWindow.FromHandle(control.Handle);
-            window.DefWndProc(ref msgResumeUpdate);
-
+            Message msgResumeUpdate = Message.Create(control.Handle, WM_SETREDRAW, new IntPtr(1), IntPtr.Zero);
+            NativeWindow.FromHandle(control.Handle).DefWndProc(ref msgResumeUpdate);
             control.Invalidate();
         }
 
+        // TODO: Accelerate this method as much as possible using async and whatever else we can do!
         private void T_Tick(object sender, EventArgs e)
         {
             if (!textChanged)
@@ -95,6 +90,11 @@ namespace WinFormsIDETest
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
             textChanged = true;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Size = this.Size - Rel;
         }
     }
 }
