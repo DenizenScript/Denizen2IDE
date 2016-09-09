b5d271c061a7408855a126204b03539bfb8c1bf6
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-09-09 16:00:17 -0700

    Initial commit: basic RTF nonsense stuff

---
+++ WinFormsIDETest/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WinFormsIDETest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Timer t = new Timer();
+            t.Interval = 1000;
+            t.Tick += T_Tick;
+            t.Start();
+            richTextBox1.AcceptsTab = true;
+            richTextBox1.HScroll += RichTextBox1_HScroll;
+        }
+
+        private void RichTextBox1_HScroll(object sender, EventArgs e)
+        {
+            textChanged = true;
+        }
+
+        private const int WM_SETREDRAW = 0x000B;
+
+        public static void Suspend(Control control)
+        {
+            Message msgSuspendUpdate = Message.Create(control.Handle, WM_SETREDRAW, IntPtr.Zero,
+                IntPtr.Zero);
+
+            NativeWindow window = NativeWindow.FromHandle(control.Handle);
+            window.DefWndProc(ref msgSuspendUpdate);
+        }
+
+        public static void Resume(Control control)
+        {
+            // Create a C "true" boolean as an IntPtr
+            IntPtr wparam = new IntPtr(1);
+            Message msgResumeUpdate = Message.Create(control.Handle, WM_SETREDRAW, wparam,
+                IntPtr.Zero);
+
+            NativeWindow window = NativeWindow.FromHandle(control.Handle);
+            window.DefWndProc(ref msgResumeUpdate);
+
+            control.Invalidate();
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            if (!textChanged)
+            {
+                return;
+            }
+            // Setup
+            Suspend(this);
+            int start = richTextBox1.SelectionStart;
+            int len = richTextBox1.SelectionLength;
+            // Update RTF
+            string text = richTextBox1.Text;
+            int py = richTextBox1.GetPositionFromCharIndex(0).Y;
+            int cLoc = 0;// (richTextBox1.GetCharIndexFromPosition(new Point(0, -Math.Max(py - 1, 0))) / 6) * 6;
+            RTFBuilder rtf = new RTFBuilder();
+            //rtf.Append(RTFBuilder.For(text.Substring(0, cLoc)));
+            int maxlen = (text.Length / 6) * 6;
+            for (int i = cLoc; i < maxlen; i += 6)
+            {
+                rtf.Append(RTFBuilder.Bold(RTFBuilder.For(text[i].ToString())));
+                rtf.Append(RTFBuilder.Italic(RTFBuilder.For(text[i + 1].ToString())));
+                rtf.Append(RTFBuilder.Colored(RTFBuilder.For(text[i + 2].ToString()), ColorTable.BLUE));
+                rtf.Append(RTFBuilder.Strike(RTFBuilder.For(text[i + 3].ToString())));
+                rtf.Append(RTFBuilder.WavyUnderline(RTFBuilder.For(text[i + 4].ToString())));
+                rtf.Append(RTFBuilder.Underline(RTFBuilder.For(text[i + 5].ToString())));
+            }
+            for (int i = maxlen; i < text.Length; i++)
+            {
+                rtf.Append(RTFBuilder.For(text[i].ToString()));
+            }
+            richTextBox1.Rtf = rtf.FinalOutput();
+            // Set back to normal
+            Resume(this);
+            richTextBox1.Select(start, len);
+            richTextBox1.Invalidate();
+            textChanged = false;
+        }
+
+        bool textChanged = false;
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            textChanged = true;
+        }
+    }
+}
---
+++ WinFormsIDETest/RTFBuilder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WinFormsIDETest
+{
+    public class RTFBuilder
+    {
+        public static RTFBuilder For(string text)
+        {
+            return new RTFBuilder() { InternalStr = "{" + text.Replace("\\", "\\\\").Replace("{", "\\{").Replace("}", "\\}").Replace("\t", "\\tab").Replace("\r", "").Replace("\n", "\\par") + "}" };
+        }
+
+        public static RTFBuilder Bold(RTFBuilder text)
+        {
+            return new RTFBuilder() { InternalStr = "{\\b " + text.ToString() + "\\b0}" };
+        }
+
+        public static RTFBuilder Italic(RTFBuilder text)
+        {
+            return new RTFBuilder() { InternalStr = "{\\i " + text.ToString() + "\\i0}" };
+        }
+
+        public static RTFBuilder WavyUnderline(RTFBuilder text)
+        {
+            return new RTFBuilder() { InternalStr = "{\\ulwave " + text.ToString() + "\\ulwave0}" };
+        }
+
+        public static RTFBuilder Strike(RTFBuilder text)
+        {
+            return new RTFBuilder() { InternalStr = "{\\strike " + text.ToString() + "\\strike0}" };
+        }
+
+        public static RTFBuilder Underline(RTFBuilder text)
+        {
+            return new RTFBuilder() { InternalStr = "{\\ul " + text.ToString() + "\\ul0}" };
+        }
+
+        public static RTFBuilder Colored(RTFBuilder text, ColorTable color)
+        {
+            return new RTFBuilder() { InternalStr = "{\\cf" + ((int)color).ToString() + " " + text.ToString() + "\\cf0}" };
+        }
+
+        public StringBuilder Internal = new StringBuilder();
+
+        string InternalStr
+        {
+            set
+            {
+                Internal.Append(value);
+            }
+        }
+
+        public Dictionary<int, string> colors;
+
+        public RTFBuilder Append(RTFBuilder builder)
+        {
+            Internal.Append(builder.ToString());
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return Internal.ToString();
+        }
+
+        public string FinalOutput()
+        {
+            return "{\\rtf1{\\colortbl ;\\red0\\green0\\blue0;\\red255\\green0\\blue0;\\red0\\green255\\blue0;\\red0\\green0\\blue255;}\\b0\\i0\\cf0" + Internal + "\\par}";
+        }
+    }
+
+    public enum ColorTable
+    {
+        BLACK = 1,
+        RED = 2,
+        GREEN = 3,
+        BLUE = 4
+    }
+}
