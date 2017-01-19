18f0cf3b7bd8587283e87ef72f9e75c835e3a16b
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2017-01-19 04:35:42 -0800

    Fix colors when hitting enter

--- Denizen2IDE/Form1.cs
+++ Denizen2IDE/Form1.cs
@@ -592,6 +592,7 @@ namespace Denizen2IDE
                 }
                 RTFBox.Text = RTFBox.Text.Substring(0, s) + Environment.NewLine + new string(' ', spaces) + RTFBox.Text.Substring(s + RTFBox.SelectionLength);
                 RTFBox.Select(s + Environment.NewLine.Length + spaces - 1, 0);
+                T_Tick(null, null);
             }
         }
 
